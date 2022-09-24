139c8f4086dbddeca1a44a9766773877accfc137
Anto <107312386+zioForas@users.noreply.github.com>
2022-09-24 21:31:42 +0100

    first commit

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,10 @@
+import { Error, Loader, SongCard } from'../components';
+import { genres } from '../assets/constants';
+
+
+const Discover = () =>{ <div>Discover</div>;
+return (
+    <div className='flex flex-col'>Discover</div>
+)
+};
+export default Discover;
